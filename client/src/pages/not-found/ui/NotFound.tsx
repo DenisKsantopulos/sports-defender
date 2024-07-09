34cce5308fc92e8505f5465a3bed75567901e983
Danil Ochagov <danilochagov@gmail.com>
2024-07-09 12:31:36 +0300

    Add 404 page

--- client/src/pages/not-found/ui/NotFound.tsx
+++ client/src/pages/not-found/ui/NotFound.tsx
@@ -1,5 +1,24 @@
+import image from '@/app/ui/assets/404.png';
+import { Link } from 'react-router-dom';
+import Paths from '@/shared/model/paths';
+import styles from './not-found.module.scss';
+
 function NotFound(): React.ReactElement {
-	return <section>404</section>;
+	return (
+		<section className={styles['not-found']}>
+			<img
+				src={image}
+				alt='404 Ошибка'
+				title='404 Ошибка'
+				loading='lazy'
+				className={styles['not-found__image']}
+			/>
+			<h2 className={styles['not-found__title']}>Страница не найдена!</h2>
+			<Link to={Paths.HOME} className={styles['not-found__link']}>
+				Перейти на главную
+			</Link>
+		</section>
+	);
 }
 
 export default NotFound;
