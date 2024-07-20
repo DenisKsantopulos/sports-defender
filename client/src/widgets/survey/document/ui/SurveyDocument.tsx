a6198ff09871e09e883c9b987643c69075c3b3da
Danil Ochagov <danilochagov@gmail.com>
2024-07-20 14:54:47 +0300

    Show document template title when a user completes the survey

--- client/src/widgets/survey/document/ui/SurveyDocument.tsx
+++ client/src/widgets/survey/document/ui/SurveyDocument.tsx
@@ -2,6 +2,9 @@ import Paths from '@/shared/model/data/Paths';
 import { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './survey-document.module.scss';
+import { useDocumentById } from '@/shared/api/queries';
+import Info from '@/widgets/common/fetch-status/info/ui/Info';
+import Error from '@/widgets/common/fetch-status/error/ui/Error';
 
 interface SurveyDocumentArgumentsType {
 	documentId: string;
@@ -11,19 +14,33 @@ const SurveyDocument = memo(
 	({ documentId }: SurveyDocumentArgumentsType): React.ReactElement => {
 		const navigate = useNavigate();
 
+		const { data, isLoading, error } = useDocumentById(documentId); // Необходимо для вывода названия шаблона документа на экран
+
+		if (isLoading) return <Info text='Загрузка шаблона документа...' />;
+
+		if (error)
+			return (
+				<Error text='При загрузке необходимого шаблона документа возникла ошибка!' />
+			);
+
 		return (
-			<p className={styles.text}>
-				По результатам анкеты вам необходим следующий{' '}
-				<a
-					className={styles.link}
-					onClick={() =>
-						navigate(`${Paths.VIEW_DOCUMENT}/${documentId}`)
-					}
-				>
-					шаблон документа
-				</a>
-				.
-			</p>
+			<>
+				{data && (
+					<p className={styles.text}>
+						По результатам анкеты вам необходим следующий шаблон
+						документа:{' '}
+						<a
+							className={styles.link}
+							onClick={() =>
+								navigate(`${Paths.VIEW_DOCUMENT}/${data.id}`)
+							}
+						>
+							{data.title}
+						</a>
+						.
+					</p>
+				)}
+			</>
 		);
 	}
 );
