bf811a3f68694f83ea4f2e81561d136eb1dd826f
Danil Ochagov <danilochagov@gmail.com>
2024-07-23 02:18:33 +0300

    Remove icon and exclamation mark from survey warning box

--- client/src/widgets/survey/header/ui/SurveyHeader.tsx
+++ client/src/widgets/survey/header/ui/SurveyHeader.tsx
@@ -1,7 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
-import styles from './survey-header.module.scss';
 import { memo } from 'react';
+import styles from './survey-header.module.scss';
 
 interface SurveyHeaderArgumentsType {
 	title: string;
@@ -15,13 +13,7 @@ const SurveyHeader = memo(
 				<h2 className={styles['survey__question']}>{title}</h2>
 				{children !== undefined && (
 					<div className={styles['survey__warning']}>
-						<h6 className={styles['warning__title']}>
-							<FontAwesomeIcon
-								icon={faCircleExclamation}
-								className={styles['warning__icon']}
-							/>
-							Справка!
-						</h6>
+						<h6 className={styles['warning__title']}>Справка</h6>
 						<p className={styles['warning__message']}>{children}</p>
 					</div>
 				)}
